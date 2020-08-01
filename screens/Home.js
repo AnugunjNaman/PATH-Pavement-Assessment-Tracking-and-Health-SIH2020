ffdd614e42f1bcb8be9d1888f1b556331778aee4
ChetanSinha <chetansinha9904@gmail.com>
2020-08-01 12:43:49 +0530

    First Commit

---
+++ screens/Home.js
@@ -0,0 +1,114 @@
+import React, { useContext , useEffect} from 'react';
+import { StyleSheet, Text, View, Image, FlatList, Alert} from 'react-native';
+import { Card, Button} from 'react-native-paper';
+import {myContext} from '../reducers/reducer';
+
+
+const Home = ({navigation}) => {
+    const {state, dispatch} = useContext(myContext);
+    const {data, loading} = state;
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const ngrok_url = "http://bfafd210d3cf.ngrok.io"
+
+    const fetchData = () => {
+        fetch(`${ngrok_url}`)
+        .then(res =>res.json())
+        .then(results => {
+            // setData(results)
+            dispatch({type:"ADD_DATA", payload:results})
+        })
+        .catch((err) => {Alert.alert("Could not load data from server.")})
+        .finally(() => (
+            //setLoading(false)
+            dispatch({type:"SET_LOADING", payload:false})
+            ))
+    }
+
+    const renderList = ((item) => {
+        return (
+            <Card
+              style={styles.myCard}
+              key={item._id.toString()}
+              onPress={() => {navigation.navigate("RoadProfile", {item})}}
+            >
+                <View style={styles.flexRow}>
+                    <View style={styles.cardView}>
+                        <View style={styles.oneUnit}>
+                            <Image source={{uri: item.imageUrls[0]}} 
+                                style= {{width: 70, height: 70, borderRadius: 35}}
+                            />
+                            <Text style={styles.text}>{item.roadName}</Text>
+                        </View>
+                    </View>
+                    <View style={{margin: 15}}>
+                        <Text>Road Number: {item.roadNumber}</Text>
+                        <Text>Chainage From: {item.fromChainage} To: {item.toChainage}</Text>
+                        <Text>PCI: {item.pci}</Text>
+                    </View>
+                </View>
+            </Card>
+        )
+    })
+    return (
+        <View style={{flex:1}}>
+            <FlatList 
+              data={data}
+              renderItem={({item}) =>{
+                  return renderList(item)
+              }}
+              keyExtractor={item => item._id.toString()}
+              refreshing={loading}
+              onRefresh={() => fetchData()}
+            />
+            <Button
+              onPress={() => navigation.navigate("Create")}
+              style={styles.button}
+              small={false}
+              mode='contained'
+              theme={theme}
+            >Add Road Details</Button>
+        </View>
+    );
+}
+
+const theme = {
+    colors:{
+        primary: "#000",
+    }
+}
+
+const styles = StyleSheet.create({
+    myCard: {
+        margin: 5,
+        padding: 5,
+        backgroundColor: '#fff'
+    },
+    cardView: {
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    flexRow: {
+        flexDirection: 'row'
+    },
+    text: {
+        color: 'black',
+        fontWeight:'bold'
+    },
+    oneUnit: {
+        marginLeft: 10,
+        justifyContent: 'center'
+    },
+    button: {
+        position: 'absolute',
+        margin: 25,
+        right: 0,
+        bottom : 0,
+        left:0,
+    }
+})
+export default Home;
---
+++ screens/ViewImage.js
@@ -0,0 +1,39 @@
+import React, {Component, useState} from 'react';
+// import ImageView from 'react-native-image-viewing';
+// import Gallery from 'react-native-image-gallery'
+import { StyleSheet, Text, View, Modal } from 'react-native';
+import ImageViewer from 'react-native-image-zoom-viewer';
+
+
+export default class ViewImage extends Component {
+
+    render(){
+        const imagesUrl = this.props.route.params.imageUrls||this.props.route.params.damageImageUrls;
+
+        let images = []
+
+        if (imagesUrl){
+            imagesUrl.forEach(image => {
+            images.push({url: image})
+            })
+        } else {
+            console.log("empty")
+        }
+
+        console.log(images.length)
+        return (
+            <ImageViewer imageUrls={images}/>
+        );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    margin: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
